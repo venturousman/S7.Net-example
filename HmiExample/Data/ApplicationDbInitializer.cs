4ff1e77042986372d11a1a9d15e7e2f91fe00475
tinhoangnguyen <tinhoang.nguyen@hotschedules.com>
2018-10-20 23:05:34 +0700

    init plans

--- HmiExample/Data/ApplicationDbInitializer.cs
+++ HmiExample/Data/ApplicationDbInitializer.cs
@@ -27,6 +27,25 @@ namespace HmiExample.Data
 
             context.Products.AddRange(defaultProducts);
 
+            // seed Employee
+            IList<Employee> defaultEmployees = new List<Employee>();
+
+            defaultEmployees.Add(new Employee() { Id = Guid.NewGuid(), Code = "E001", Email = "employee1@gmail.com", DisplayName = "Employee 1", FirstName = "A", MidleName = "Van", LastName = "Tran" });
+            defaultEmployees.Add(new Employee() { Id = Guid.NewGuid(), Code = "E002", Email = "employee2@gmail.com", DisplayName = "Employee 2", FirstName = "B", MidleName = "Van", LastName = "Nguyen" });
+
+            context.Employees.AddRange(defaultEmployees);
+
+            // seed Plan
+            IList<Plan> defaultPlans = new List<Plan>();
+
+            defaultPlans.Add(new Plan() { Id = Guid.NewGuid(), MachineId = defaultMachines[0].Id, ProductId = defaultProducts[1].Id, EmployeeId = defaultEmployees[1].Id, ExpectedQuantity = 32 });
+            defaultPlans.Add(new Plan() { Id = Guid.NewGuid(), MachineId = defaultMachines[0].Id, ProductId = defaultProducts[1].Id, EmployeeId = defaultEmployees[0].Id, ExpectedQuantity = 13 });
+            defaultPlans.Add(new Plan() { Id = Guid.NewGuid(), MachineId = defaultMachines[0].Id, ProductId = defaultProducts[0].Id, EmployeeId = defaultEmployees[0].Id, ExpectedQuantity = 17 });
+            defaultPlans.Add(new Plan() { Id = Guid.NewGuid(), MachineId = defaultMachines[1].Id, ProductId = defaultProducts[0].Id, EmployeeId = defaultEmployees[1].Id, ExpectedQuantity = 41 });
+            defaultPlans.Add(new Plan() { Id = Guid.NewGuid(), MachineId = defaultMachines[1].Id, ProductId = defaultProducts[1].Id, EmployeeId = defaultEmployees[0].Id, ExpectedQuantity = 22 });
+
+            context.Plans.AddRange(defaultPlans);
+
             base.Seed(context);
         }
     }
